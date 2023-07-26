3f25f4f6ebea9ef8524eed726298bc39e9306eef
Michelle Baird <michelle.baird@fishtownanalytics.com>
2023-07-26 15:43:09 +0000

    supid useless change

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -18,13 +18,7 @@ order_payments as (
 final as (
 
     select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
+        orders.order_id
 )
 
 select * from final
